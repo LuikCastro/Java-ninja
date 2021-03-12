ce7a45396788dc41aaeef4a4221837ec1c9e5c41
LuikCastro <luik.silva@bandtec.com.br>
2021-03-12 19:14:17 -0300

    criando primeiro objeto

--- LP-05-Exercicios/src/main/java/com/mycompany/lp/exercicios/TesteTermometro.java
+++ LP-05-Exercicios/src/main/java/com/mycompany/lp/exercicios/TesteTermometro.java
@@ -10,5 +10,15 @@ package com.mycompany.lp.exercicios;
  * @author castr
  */
 public class TesteTermometro {
-    
+    public static void main(String[] args) {
+        Termometro TermometroUm = new Termometro();
+        
+        TermometroUm.temperaturaAtual = 24.0;
+        TermometroUm.temperaturaMax = 35.0;
+        TermometroUm.temperaturaMin = 18.0;
+        
+        TermometroUm.aumentaTemperatura(5.0);
+        TermometroUm.diminuiTemperatura(20.0);
+        TermometroUm.exibeFahreinheit();
+    }
 }
