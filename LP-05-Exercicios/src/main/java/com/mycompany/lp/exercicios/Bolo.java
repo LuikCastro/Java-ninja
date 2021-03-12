1ac8dc4a41df1d1594da48bc5265bf7b9611b951
LuikCastro <luik.silva@bandtec.com.br>
2021-03-12 20:53:49 -0300

    terminei método comprarBolo e exibirRelatorio

---
+++ LP-05-Exercicios/src/main/java/com/mycompany/lp/exercicios/Bolo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lp.exercicios;
+
+/**
+ *
+ * @author castr
+ */
+public class Bolo {
+    String sabor;
+    Double valor;
+    Integer quantidadeVenda;
+    
+    public void comprarBolo(Integer quantidadeBolo){
+        Integer quantidadeValidacao = quantidadeVenda + quantidadeBolo;
+        if (quantidadeValidacao < 100){
+            quantidadeVenda += quantidadeBolo;
+        }
+        else{
+            System.out.println("Seu pedido ultrapassou nosso limite diário para este bolo");
+        }
+    }
+    public void exibirRelatori(String sabor, Double valor, Integer quantidadeVenda){
+        System.out.println(String.format("O bolo de %s, foi comprado %d vezes, totalizando R$%.2f", sabor, quantidadeVenda, valor));
+    }
+}
