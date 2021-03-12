f167c33bfd50b323a74f167982992fbadc27a8a8
LuikCastro <luik.silva@bandtec.com.br>
2021-03-12 18:34:27 -0300

    refazendo exercicio 1 - inicio

--- LP-05-Exercicios/src/main/java/com/mycompany/lp/exercicios/Termometro.java
+++ LP-05-Exercicios/src/main/java/com/mycompany/lp/exercicios/Termometro.java
@@ -4,43 +4,17 @@
  * and open the template in the editor.
  */
 package com.mycompany.lp.exercicios;
-
-import java.util.Scanner;
-
 /**
  *
  * @author castr
  */
 public class Termometro { 
-    public static Double aumentaTemperatura(Double temperaturaNova, Double temperaturaAtual){
-        Double resultado = 0.0;
-        if (temperaturaNova > temperaturaAtual){
-            resultado = temperaturaNova;
-        }
-        else{
-            resultado = temperaturaAtual;
-        }
-        return resultado;
+    
+    public static void aumentaTemperatura(){
+        
     }
     
-    public static Double diminuiTemperatura(Double temperaturaNova, Double temperaturaAtual){
-        Double resultado = 0.0;
-        if (temperaturaNova < temperaturaAtual){
-            resultado = temperaturaNova;
-        }
-        else{
-            resultado = temperaturaAtual;
-        }
-        return resultado;
-    }
-    
-    public static void main(String[] args) {
-        Double temperaturaAtual, temperaturaMax, temperaturaMin;
-        Scanner leitor = new Scanner(System.in);
-        System.out.println("Digite uma temperatura");
-        temperaturaAtual = leitor.nextDouble();
-        System.out.println("Digite outra temperatura");
-        temperaturaMax = aumentaTemperatura(leitor.nextDouble(), temperaturaAtual);
-        temperaturaMin = diminuiTemperatura(leitor.nextDouble(), temperaturaAtual);
+    public static void diminuiTemperatura(){
+        
     }
 }
