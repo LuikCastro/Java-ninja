ff53e237e8365f14749b06afe326b3382e0411ff
LuikCastro <luik.silva@bandtec.com.br>
2021-03-17 19:05:50 -0300

    criação atributos ex2

---
+++ LP-06-Exercicios/src/main/java/com/mycompany/lp/exercicios/Encomendas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lp.exercicios;
+
+/**
+ *
+ * @author castr
+ */
+public class Encomendas {
+    Double altura;
+    Double largura;
+    String enderecoRemetente;
+    String enderecoDestinatario;
+    Double distancia;
+    Double valorEncomenda;
+}
