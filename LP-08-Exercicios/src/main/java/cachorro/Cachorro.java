b8ed466cf2d74c0620b7c276a8b5433e591c49d5
LuikCastro <luik.silva@bandtec.com.br>
2021-04-02 00:21:50 -0300

    classe Cachorro finalizada

--- LP-08-Exercicios/src/main/java/cachorro/Cachorro.java
+++ LP-08-Exercicios/src/main/java/cachorro/Cachorro.java
@@ -13,4 +13,45 @@ public class Cachorro {
     String apelido;
     String porte;
     Double peso;
+    
+    public Cachorro() {
+        
+    }
+    
+    public Cachorro (String apelido, String porte, Double peso){
+        this.apelido = apelido;
+        this.porte = porte;
+        this.peso = peso;
+    }
+    
+    public void Comer(String nomeComida, Double quantidadeKg){
+        if (quantidadeKg > 0){
+            if ("Pequeno".equals(this.porte) && this.peso < 15.00){
+                System.out.println("Cão comeu " + quantidadeKg + "kg de " + nomeComida);
+                this.peso = this.peso + ((30 / 100) * quantidadeKg);
+            }
+            else{
+                System.out.println("Cão obeso demais, não vai aceitar a comida");
+            }
+            if ("Medio".equals(this.porte) && this.peso < 30.00){
+                System.out.println("Cão comeu " + quantidadeKg + "kg de " + nomeComida);
+                this.peso = this.peso + ((30 / 100) * quantidadeKg);
+            }
+            else{
+                System.out.println("Cão obeso demais, não vai aceitar a comida");
+            }
+            if ("Grande".equals(this.porte) && this.peso < 45.00){
+                System.out.println("Cão comeu " + quantidadeKg + "kg de " + nomeComida);
+                this.peso = this.peso + ((30 / 100) * quantidadeKg);
+            }
+            else{
+                System.out.println("Cão obeso demais, não vai aceitar a comida");
+            }
+        }
+        else{
+            System.out.println("Quantidade inválida");
+        }
+    }
 }
+
+
